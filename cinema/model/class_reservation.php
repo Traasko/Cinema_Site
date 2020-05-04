554d6faf4ace4efe6da78e9f0c7bf457b6b59428
Traasko <56584321+Traasko@users.noreply.github.com>
2020-05-04 12:21:11 +0200

    reservation

--- cinema/model/class_reservation.php
+++ cinema/model/class_reservation.php
@@ -3,30 +3,24 @@
 class Reservation{
 
   private $nom;
-  private $phone;
   private $date;
   private $heur;
   private $film;
 
-  public function __construct($nom, $phone, $date, $heur, $film){
+  public function __construct($nom, $date, $heur, $film){
       $this->setNom($nom);
-      $this->setPhone($phone);
       $this->setDate($date);
       $this->setHeur($heur);
       $this->setfilm($film);
     }
 
     public function getNom(){ return $this->nom; }
-    public function getPhone(){ return $this->phone; }
     public function getDate(){ return $this->date; }
     public function getHeur(){ return $this->heur; }
     public function getfilm(){ return $this->film; }
 
     public function setNom($nom){
         $this->nom = $nom;
-    }
-    public function setPhone($phone){
-        $this->phone = $phone;
     }
     public function setDate($date){
         $this->date = $date;
--- cinema/traitement/traitement_reservation.php
+++ cinema/traitement/traitement_reservation.php
@@ -3,7 +3,7 @@
 require('../manager/manager.php');
 require('../model/class_reservation.php');
 
-$reservation = new Reservation($_POST['nom'], $_POST['phone'], $_POST['date'], $_POST['heur'], $_POST['film']);
+$reservation = new Reservation($_POST['nom'], $_POST['date'], $_POST['heur'], $_POST['film']);
 $manager = new Manager();
 $manager->reservation($reservation);
 
