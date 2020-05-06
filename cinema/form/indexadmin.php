578eb8578e4c1b1be126d560b7b61e2db55cbb1e
ryanftne <ryan-fontaine@hotmail.fr>
2020-05-06 16:16:31 +0200

    page admin

--- cinema/form/indexadmin.php
+++ cinema/form/indexadmin.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <title>Projet cinéma</title>
-    <title>Site cinéma</title>
+    <title>Page admin</title>
+    <title>Page admin</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
